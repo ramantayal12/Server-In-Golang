0da7a7285cd3208037fcb7a27e85249b98804f68
Raman Tayal <ramantayal12@gmail.com>
2024-04-07 23:27:58 +0530

    added basic files

---
+++ 3-strings.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// using var key word
+	var str1 = "hello world 1"
+
+	// using short hand notation
+	str2 := "hello world 2"
+
+	fmt.Println(str1)
+	fmt.Println(str2)
+
+	// length of string
+	fmt.Println(len(str1))
+}
